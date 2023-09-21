d073610a6aa8c93bc957a640700d2793a43d4b11
MohamedTedid1234 <mohamedtedid3@gmail.com>
2023-09-21 06:50:43 -0700

    a

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,11 +1,48 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_buffer - add 2 strings.
- * @b: string1.
- * @size: string2.
- * Return: String with all letters in ROT13 base.
+ * print_buffer - prints a buffer
+ *
+ * @b: char pointer
+ *
+ * @size: int
+ *
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
 
-}
+	int i, j;
+
+	i = 0;
+
+	while (i < size)
+	{
+		printf("%08x: ", i);
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+				printf("%02x", (unsigned char)b[j]);
+			else
+				printf("  ");
+			if (j % 2)
+				putchar(' ');
+		}
+		for (j = i; j < i + 10; j++)
+		{
+			if (j >= size)
+				break;
+
+			if (b[j] >= ' ' && *(b + j) <= '~')
+				putchar(*(b + j));
+			else
+				putchar('.');
+		}
+		i += 10;
+		if (i < size)
+			putchar('\n');
+	}
+	putchar('\n');
+}}
