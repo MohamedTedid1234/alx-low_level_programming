cedddec822808fed04a907b09965363ade0cbd8b
MohamedTedid1234 <mohamedtedid3@gmail.com>
2023-10-11 09:59:28 -0700

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,19 +2,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: is pointer
+ * print_name - prints a name
+ * @name: the string name
+ * @f: the printing function pointer
+ *
+ * Return: void
 **/
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	else
-	{
+	if (name && f)
 		f(name);
-	}
 }
